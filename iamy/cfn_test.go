87c3becce934af52b7f2fa11ba22ce09c177d399
Manh Ha <manh.ha@envato.com>
2025-03-13 16:22:38 +1300

    Update the test

--- iamy/cfn_test.go
+++ iamy/cfn_test.go
@@ -33,10 +33,10 @@ func TestCfnMangedResources(t *testing.T) {
 		if !cfn.IsManagedResource(CfnIamPolicy, "foobar-ABCDEFabcde12345") {
 			t.Fatal("names with id suffix are managed")
 		}
-		if !cfn.IsManagedResource(CfnIamPolicy, "foobar-Production") {
+		
+		if cfn.IsManagedResource(CfnIamPolicy, "foobar-Production") {
 			t.Fatal("names with id suffix are managed")
 		}
-
 		if cfn.IsManagedResource(CfnIamPolicy, "foobar-abcdefgh1234567") {
 			t.Fatal("names with suffix containing only lowercase letters are not managed")
 		}
